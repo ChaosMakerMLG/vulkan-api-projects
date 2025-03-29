a92787b4a30e9a90752dbc9e10f4ba6670ee6ae9
Kamil <chaosmakermlg@gmail.com>
2025-03-29 14:04:58 +0100

    commit

--- shitengine1/main.cpp
+++ shitengine1/main.cpp
@@ -151,6 +151,7 @@ class TriangleApplication {
         bool framebufferResized = false;
 
         VkBuffer vertexBuffer;
+        VkDeviceMemory vertexBufferMemory;
 
         void initWindow() {
             glfwInit();
@@ -195,18 +196,43 @@ class TriangleApplication {
             bufferInfo.usage = VK_BUFFER_USAGE_VERTEX_BUFFER_BIT;
             bufferInfo.sharingMode = VK_SHARING_MODE_EXCLUSIVE;
 
-            if (vkCreateBuffer(device, &bufferInfo, nullptr, &vertexBuffer)VK_SUCCESS) {
+            if (vkCreateBuffer(device, &bufferInfo, nullptr, &vertexBuffer) != VK_SUCCESS) {
                 throw std::runtime_error("failed to create vertex buffer");
             }
 
             VkMemoryRequirements memRequirements{};
             vkGetBufferMemoryRequirements(device, vertexBuffer, &memRequirements);
 
+            VkMemoryAllocateInfo allocInfo{};
+
+            allocInfo.sType = VK_STRUCTURE_TYPE_MEMORY_ALLOCATE_INFO;
+            allocInfo.allocationSize = memRequirements.size;
+            allocInfo.memoryTypeIndex = findMemoryType(memRequirements.memoryTypeBits, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT);
+
+            if (vkAllocateMemory(device, &allocInfo, nullptr, &vertexBufferMemory) != VK_SUCCESS) {
+                throw std::runtime_error("failed to allocate memory to vertex buffer");
+            }
+
+            vkBindBufferMemory(device, vertexBuffer, vertexBufferMemory, 0);
+
+            void* data;
+            vkMapMemory(device, vertexBufferMemory, 0, bufferInfo.size, 0, &data);
+                memcpy(data, vertices.data(), (size_t) bufferInfo.size);
+            vkUnmapMemory(device, vertexBufferMemory);
+
+
         }
 
-        uint32_t findMemoryType(uint32_t typeFilter, VkMemoryPrppertyFlags, properties) {
+        uint32_t findMemoryType(uint32_t typeFilter, VkMemoryPropertyFlags properties) {
             VkPhysicalDeviceMemoryProperties memProperties;
-            vkGetPhysicalDeviceMemoryProperties(physicalDevice, &memProperties)
+            vkGetPhysicalDeviceMemoryProperties(physicalDevice, &memProperties);
+
+            for (uint32_t i = 0; i < memProperties.memoryTypeCount; i++) {
+                if ((typeFilter & (1 << i)) && ((memProperties.memoryTypes[i].propertyFlags & properties) == properties)) {
+                    return i;
+                }
+            }
+            throw std::runtime_error("failed to find suitable memory type");
         }
 
         void createSyncObjects() {
@@ -273,7 +299,11 @@ class TriangleApplication {
             scissor.extent = swapChainExtent;
             vkCmdSetScissor(commandBuffer, 0, 1, &scissor);
 
-            vkCmdDraw(commandBuffer, 3, 1, 0, 0);
+            VkBuffer vertexBuffers[] = {vertexBuffer};
+            VkDeviceSize offsets[] = {0};
+            vkCmdBindVertexBuffers(commandBuffer, 0, 1, vertexBuffers, offsets);
+
+            vkCmdDraw(commandBuffer, static_cast<uint32_t>(vertices.size()), 1, 0, 0);
             vkCmdEndRenderPass(commandBuffer);
 
             if(vkEndCommandBuffer(commandBuffer) != VK_SUCCESS) {
@@ -850,6 +880,7 @@ class TriangleApplication {
             cleanupSwapChain();
             
             vkDestroyBuffer(device, vertexBuffer, nullptr);
+            vkFreeMemory(device, vertexBufferMemory, nullptr);
             vkDestroyPipelineLayout(device, pipelineLayout, nullptr);
             vkDestroyPipeline(device, graphicsPipeline, nullptr);
 
